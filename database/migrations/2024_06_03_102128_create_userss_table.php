1505832cbfdc2a38096a0295564b0d914dfb4d29
GgnFzn <162352596+GgnFzn@users.noreply.github.com>
2024-06-03 17:35:40 +0700

    menambahkan crud barang

--- database/migrations/2024_06_03_102128_create_userss_table.php
+++ database/migrations/2024_06_03_102128_create_userss_table.php
@@ -4,11 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUserssTable extends Migration
 {
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('userss', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -19,6 +19,6 @@ class CreateUsersTable extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('userss');
     }
 }
